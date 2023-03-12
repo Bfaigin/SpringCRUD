b0934461ddbbb584801531234796c37cb1872430
CeoOfMeme <pmax2411p@gmail.com>
2023-03-12 23:21:43 +0500

    Files added

---
+++ src/test/java/com/springproject/crudwebapp/CrudWebAppTests.java
@@ -0,0 +1,13 @@
+package com.springproject.crudwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
